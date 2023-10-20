7c0c1fc121e5425e404a1e80c64a6dc26e158e9a
Marvin Huitz <mhuitzc1@miumg.edu.gt>
2023-10-20 15:03:39 -0600

    Api casi terminada

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/login');
 //const { verificarToken } = require('../middleware/auth'); 
-//const extractDbConfig = require('../middleware/decodeToken'); 
+
 
 // Rutas para login
 router.post('/', loginController.iniciarSesion);
